45b4c92d9454c33e90eeed45c095aebb3bb7f5c6
Irakli <koregm@gmail.com>
2022-02-11 23:30:10 +0400

    Connect with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { errorHandler } = require("./middleware/errorMiddleware");
+
+// Connect to mongo database
+const connectDB = require("./config/db");
+connectDB();
 
 const app = express();
 
@@ -13,6 +18,6 @@ app.use(express.urlencoded({ extended: false })); // <-- url encode
 app.use("/api/goals", require("./routes/goalRoutes"));
 
 // overwrite the default express error handler with custom error handler middleware
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
