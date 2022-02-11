1cd341d4f618acf662a5c01b4cbc80260d8f5dac
Irakli <koregm@gmail.com>
2022-02-11 15:29:14 +0400

    Creating a Controller

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,31 @@
+// @desc Get all goals
+// @route GET /api/goals
+// @access Private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "Get Goals" });
+};
+// @desc Set goal
+// @route PUT /api/goals/:id
+// @access Private
+const setGoals = (req, res) => {
+  res.status(200).json({ message: "Set Goals" });
+};
+// @desc  Update Goal
+// @route GET /api/goals
+// @access Private
+const updateGoals = (req, res) => {
+  res.status(200).json({ message: `Update Goal ${req.params.id}` });
+};
+// @desc Delete goal
+// @route DELETE /api/goals/:id
+// @access Private
+const deleteGoals = (req, res) => {
+  res.status(200).json({ message: `Delete Goal ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoals,
+  updateGoals,
+  deleteGoals,
+};
