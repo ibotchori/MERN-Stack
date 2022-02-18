92a6cd28f2aae76f22813e94f19ad476dd6e2c25
Irakli <koregm@gmail.com>
2022-02-18 14:02:57 +0400

    Restrict the Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+  // get user from global state
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    // if user not logged in, navigate to login page
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
   return <div>Dashboard</div>;
 };
 
